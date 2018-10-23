cc882b749578f4bb82e1017f8084d51450f666dc
Ethan Barrett <hello@buildingsareheavy.com>
2018-10-22 21:30:22 -0600

    auto-reformatted files.

--- js/post.js
+++ js/post.js
@@ -1,24 +1,29 @@
 $(document).ready(function() {
+  $("#header-img, .language")
+    .delay(500)
+    .fadeIn(2000);
 
-  $('#header-img, .language').delay(500).fadeIn(2000);
-
-  $(".lang").click(function(){
+  $(".lang").click(function() {
     $("section").removeClass("hidden");
 
-    $('html,body').delay(400).animate({
-        scrollTop: $("#top").offset().top}, 1100);
-});
+    $("html,body")
+      .delay(400)
+      .animate(
+        {
+          scrollTop: $("#top").offset().top
+        },
+        1100
+      );
+  });
 
-
-  $('.lang').on('click', function(e) {
+  $(".lang").on("click", function(e) {
     e.preventDefault();
     var url = this.href;
 
-    $('#more').remove();
-    $('#all').load(url +' #more').hide().fadeIn('slow');
-  }
-
-  );
-
-
+    $("#more").remove();
+    $("#all")
+      .load(url + " #more")
+      .hide()
+      .fadeIn("slow");
+  });
 });
